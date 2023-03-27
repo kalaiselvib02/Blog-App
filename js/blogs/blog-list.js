ee6c9f89afc0aefbda4320e7a51b83635b1775ca
kalaiselvib02 <kalaiselvi.balu@cdw.com>
2023-03-27 23:35:46 +0530

    Review comments & blog detail

--- js/blogs/blog-list.js
+++ js/blogs/blog-list.js
@@ -5,18 +5,78 @@ import {APP_CONSTANTS} from "../constants/constants.js"
 const BLOG_URL = APP_CONSTANTS.FETCH_DATA.BLOG
 
 
+
+
 export async function createBlogData() {
-const blogListWrapper = document.getElementById("blog-list-wrapper")
- let blogList = await fetchData(BLOG_URL);
- console.log(blogList)
- blogList.forEach(blogItem => {
-    blogListWrapper.innerHTML += `
-    <div class="blog-list-item">
-        <div class="blog-heading">${blogItem.title}</div>
-        <div class="blog-type text-primary text-sm">${blogItem.type.toUpperCase()}</div>
-        <div class="blog-description text-truncate para-text">${blogItem.details}</div>
-    </div>
-`
- });
+
+    const blogListWrapper = document.getElementById("blogListWrapper")
+    let blogList = await fetchData(BLOG_URL);
+
+     getBlogDetails(blogList , 0)
+
+    const blogListFragment = document.createDocumentFragment();
+   
+    blogList.forEach((blogItem , index) => {
+
+    
+    const blogListItem = document.createElement("div");
+    blogListItem.className = "blog-list-item"; 
+
+    blogListItem.addEventListener("click", function () {
+        setActiveBlog(blogList , index)
+    }, false);
+
+    const blogListHeading = document.createElement("div");
+    blogListHeading.textContent= blogItem.title;
+    blogListHeading.className = "blog-heading"
+
+
+    const blogListType = document.createElement("div");
+    blogListType.textContent = blogItem.type.toUpperCase()
+    blogListType.className = "blog-type text-primary text-sm";
+
+    const blogListDescription = document.createElement("p");
+    blogListDescription.textContent = blogItem.details;
+    blogListDescription.className = "blog-description text-truncate para-text";
+
+
+
+    blogListItem.appendChild(blogListHeading);
+    blogListItem.appendChild(blogListType);
+    blogListItem.appendChild(blogListDescription)
+    blogListFragment.appendChild(blogListItem)
+
+    });
+
+    blogListWrapper.appendChild(blogListFragment);
+
+}
+
+ function setActiveBlog (list , index) {
+    const currentBlogItem = index
+   getBlogDetails(list , currentBlogItem)
+}
+
+
+
+export function  getBlogDetails(list , currentBlogItem) {
+     const blogDetailsWrapper = document.getElementById("blogDetailsWrapper");
+     const blogListDetail = currentBlogItem > 0 ? list[currentBlogItem] : list[0]
+     blogDetailsWrapper.innerHTML = `
+        <img src=${blogListDetail.photo} class="blog-detail-img mb-1"></img>
+        <h1 class="blog-detail-heading mb-1">${blogListDetail.title}</h1>
+        <p class="blog-detail-description para-text">${blogListDetail.details}</p>
+     `
+    const blogEdit = document.createElement("button");
+    blogEdit.className = "btn"
+    blogEdit.textContent = APP_CONSTANTS.FORM_INPUTS.EDIT_BLOG_FORM.SUBMIT_BTN_TEXT
+
+    blogDetailsWrapper.appendChild(blogEdit)
+    
+}
+
+
+function addBlogItem() {
+    
 }
 
--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 
 import { APP_CONSTANTS } from "./constants/constants.js";
- import { createBlogData } from "./blogs/blog-list.js";
+import { createBlogData } from "./blogs/blog-list.js";
 
- const APP_TITLE = APP_CONSTANTS.APP_TITLE;
+const APP_TITLE = APP_CONSTANTS.APP_TITLE;
 
+    const getFilterList = () => {
+        const filterList = document.getElementById("filterList");
+        const filterListOptions = APP_CONSTANTS.FILTER_DETAILS.FILTER_OPTIONS;
 
-    
-   
+        filterListOptions.forEach(item => {
+            filterList.innerHTML += `
+            <div class="input-item flex-center-align mb-1">
+                <input type="checkbox" value=${item.value} checked=${item.isChecked}></input>
+                <label>${item.name}</label>
+            </div>
+    `
+        });
+    }
    
     export const fetchData = async (url) => {  
         let data = []
@@ -17,11 +27,16 @@ import { APP_CONSTANTS } from "./constants/constants.js";
             console.log(err);
         }
         return data
-   }
+    }
 
- 
+    
+
+
+   getFilterList()
 
    createBlogData()
+
+ 
 
 
 
